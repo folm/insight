e1217e328709fd5ef164ba5bb97561f915c5484f
Folm <dev@folm.io>
2018-03-27 18:09:30 +0300

    Update for folm

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
-var defaultCurrency = localStorage.getItem('insight-currency') || 'BTC';
+var defaultCurrency = localStorage.getItem('insight-currency') || 'FLM';
 
 angular.module('insight',[
   'ngAnimate',
@@ -15,7 +15,6 @@ angular.module('insight',[
   'angularMoment',
   'insight.system',
   'insight.socket',
-  'insight.api',
   'insight.blocks',
   'insight.transactions',
   'insight.address',
@@ -28,7 +27,6 @@ angular.module('insight',[
 
 angular.module('insight.system', []);
 angular.module('insight.socket', []);
-angular.module('insight.api', []);
 angular.module('insight.blocks', []);
 angular.module('insight.transactions', []);
 angular.module('insight.address', []);
--- public/src/js/controllers/currency.js
+++ public/src/js/controllers/currency.js
@@ -20,15 +20,15 @@ angular.module('insight.currency').controller('CurrencyController',
 
         if (this.symbol === 'USD') {
           response = _roundFloat((value * this.factor), 2);
-        } else if (this.symbol === 'mBTC') {
+        } else if (this.symbol === 'mFLM') {
           this.factor = 1000;
           response = _roundFloat((value * this.factor), 5);
-        } else if (this.symbol === 'bits') {
+        } else if (this.symbol === 'folms') {
           this.factor = 1000000;
           response = _roundFloat((value * this.factor), 2);
-        } else { // assumes symbol is BTC
+        } else {
           this.factor = 1;
-          response = _roundFloat((value * this.factor), 8);
+          response = value;
         }
         // prevent sci notation
         if (response < 1e-7) response=response.toFixed(8);
@@ -47,9 +47,9 @@ angular.module('insight.currency').controller('CurrencyController',
         Currency.get({}, function(res) {
           $rootScope.currency.factor = $rootScope.currency.bitstamp = res.data.bitstamp;
         });
-      } else if (currency === 'mBTC') {
+      } else if (currency === 'mFLM') {
         $rootScope.currency.factor = 1000;
-      } else if (currency === 'bits') {
+      } else if (currency === 'folms') {
         $rootScope.currency.factor = 1000000;
       } else {
         $rootScope.currency.factor = 1;
